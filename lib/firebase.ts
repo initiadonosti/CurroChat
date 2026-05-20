aa73c1585843d177ef523e99dd97b917f02efae1
AsierKarrera <asikarfer@gmail.com>
2026-05-20 11:04:18 +0200

    switch firebase to new project

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,15 +5,21 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEdGGdttI6m3e5Fl61HBl8n6HpcGjT4Hk",
-  authDomain: "chathorarios.firebaseapp.com",
-  databaseURL:
-    "https://chathorarios-default-rtdb.firebaseio.com",
-  projectId: "chathorarios",
-  storageBucket: "chathorarios.appspot.com",
-  messagingSenderId: "365124256259",
-  appId: "1:365124256259:web:ffa70861acdce20fc58436",
+
+  apiKey: "AIzaSyB8oA66-xZNmTIH-oRj9FRlLSegn_LmElM",
+
+  authDomain: "curro-chat-app.firebaseapp.com",
+
+  projectId: "curro-chat-app",
+
+  storageBucket: "curro-chat-app.firebasestorage.app",
+
+  messagingSenderId: "41530793050",
+
+  appId: "1:41530793050:web:b751bbc52b8a6f63898c63"
+
 };
+
 
 const app =
   getApps().length > 0
